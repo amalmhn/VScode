eb7a86d7540767d9793b39dea5970cd5aaab29cb
Amal M <amalmhnofficial@gmail.com>
2021-01-20 23:29:31 +0530

    vsCode programs

---
+++ cssmod/javaScriptprograms/helloworld.js
@@ -0,0 +1,15 @@
+var companyname="luminar Technolab";
+// var,let,const are the variable declaration in js
+console.log(companyname);
+// command for print
+//You can check the result in the browser terminal in linked html page
+// there is no special method for reading from console
+var num=10;//int
+var num2=10.5;//float //there is no float seperation in js. So it will take as number
+var isTrue=false;//boolean
+var name1="Test"//str
+
+console.log(typeof(num))//number
+console.log(typeof(num2))//number
+console.log(typeof(isTrue))//boolean
+console.log(typeof(name1))//string
